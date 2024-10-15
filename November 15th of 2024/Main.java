a37fc18db3c50e9cd895308b9549ebeddcfaecc1
kvacho <kvachadzegiorgi10@outlook.com>
2024-10-15 19:40:06 +0400

    5 Done

---
+++ November 15th of 2024/Main.java
@@ -0,0 +1,48 @@
+public class Main{
+  public static void main(String args[]) {
+    /// N1
+    // byte a = 10;
+    // byte b = 27;
+    // System.out.println(a + " " + b);
+    // byte c = a;
+    // a = b;
+    // b = c;
+    // System.out.println("reversed: " + a + " " + b);
+    /// N1 no third variable
+    // byte a = 10;
+    // byte b = 27;
+    // System.out.println(a + " " + b);
+    // a = (byte)(a + b);
+    // b = (byte)(a - b);
+    // a = (byte)(a - b);
+    // System.out.println("reversed: " + a + " " + b);
+    /// N2
+    // float a = 10.234F;
+    // int b = (int) a;
+    // System.out.println(a % b);
+    /// N3
+    // byte a = 123;
+    // boolean isWhatever = false;
+    // if (a % 100 != 0 | a == 100) {
+      // isWhatever = true;
+    // }
+    // System.out.println(isWhatever);
+    /// N4
+    // char a = 'a';
+    // if (a >= 'a' && a <= 'z') {
+      // a = (char)(a - 32); // ASCII viyeneb
+    // }
+    // System.out.println(a);
+    /// N5
+    // float a = 12.134F;
+    // int b = (int) a;
+    // int c;
+    // if (a % b >= 0.5) {
+      // c = b + 1;
+    // }
+    // else {
+      // c = b;
+    // }
+    // System.out.println(c);
+  }
+}
