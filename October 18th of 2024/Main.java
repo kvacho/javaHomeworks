6eda754e918869e989647c61ab2c5496d01cee19
kvacho <kvachadzegiorgi10@outlook.com>
2024-10-18 19:45:16 +0400

    work bro i beg

---
+++ October 18th of 2024/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        // N1
+        Scanner input = new Scanner(System.in); // უკომენტაროდ დატოვებულია რადგან ერთ ობიექტს ვიყენებ ყველგან
+//        System.out.println("Enter celsius to convert to fahrenheit: ");
+//        int c = input.nextInt();
+//        c = (c * 9/5) + 32;
+//        System.out.println(c);
+        // N2
+//        System.out.println("Enter fahrenheit to convert to celsius: ");
+//        int f = input.nextInt();
+//        f = (f - 32) * 5/9;
+//        System.out.println(f);
+        // N3
+//        System.out.println("Enter year: ");
+//        int y = input.nextInt();
+//        if (y % 4 == 0 && y % 100 != 0) {
+//            System.out.println("True;");
+//        }
+//        else if (y % 4 == 0 && y % 200 != 0) {
+//            System.out.println("True;");
+//        }
+//        else if (y % 4 == 0 && y % 400 == 0) {
+//            System.out.println("True;");
+//        }
+//        else {
+//            System.out.println("False;");
+//        }
+        // N4
+//        System.out.println("Enter x and then y: ");
+//        int x = input.nextInt();
+//        int y = input.nextInt();
+//        if (x > 0 && y > 0) {
+//            System.out.println(1);
+//        }
+//        else if (x < 0 && y > 0) {
+//            System.out.println(2);
+//        }
+//        else if (x < 0 && y < 0) {
+//            System.out.println(3);
+//        }
+//        else if (x > 0 && y < 0) {
+//            System.out.println(4);
+//        }
+//        else {
+//            System.out.println("The point is on one of the axis.");
+//        }
+        // N5
+        // იმის დაშვებით, რომ ჯერ სტრინგის შემოსვლა აუცილებელია
+//        String op = input.nextLine();
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        if (op.equals("+")) {
+//            System.out.println("Result: " + (a + b));
+//        } else if (op.equals("-")) {
+//            System.out.println("Result: " + (a - b));
+//        } else if (op.equals("*")) {
+//            System.out.println("Result: " + (a * b));
+//        } else if (op.equals("/")) {
+//            if (b != 0) {
+//                System.out.println("Result: " + (a / b));
+//            } else {
+//                System.out.println("Cannot divide by 0.");
+//            }
+//        } else {
+//            System.out.println("Please recheck your spelling, only plus, minus, mult and div are allowed.");
+//        }
+    }
+}
