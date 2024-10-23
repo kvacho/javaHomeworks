5aceef2456a5e77ad191b67834a833f4eb1f84c5
kvacho <kvachadzegiorgi10@outlook.com>
2024-10-23 19:45:12 +0400

    Changed misleading date

---
+++ October 15th of 2024/Main.java
@@ -0,0 +1,75 @@
+public class Main{
+  public static void main(String args[]) {
+    /// N1
+    // byte a = 10;
+    // byte b = 27;
+    // System.out.println(a + " " + b);
+    // byte c = a;
+    // a = b;
+    // b = c;
+    // System.out.println("reversed: " + a + " " + b);
+    /// N1 no third variable
+    // byte a = 10;
+    // byte b = 27;
+    // System.out.println(a + " " + b);
+    // a = (byte)(a + b);
+    // b = (byte)(a - b);
+    // a = (byte)(a - b);
+    // System.out.println("reversed: " + a + " " + b);
+    /// N2
+    // float a = 10.234F;
+    // int b = (int) a;
+    // System.out.println(a % b);
+    /// N3
+    // byte a = 123;
+    // boolean isWhatever = false;
+    // if (a % 100 != 0 | a == 100) {
+      // isWhatever = true;
+    // }
+    // System.out.println(isWhatever);
+    /// N4
+    // char a = 'a';
+    // if (a >= 'a' && a <= 'z') {
+      // a = (char)(a - 32); // ASCII viyeneb
+    // }
+    // System.out.println(a);
+    /// N5
+    // float a = 12.134F;
+    // int b = (int) a;
+    // int c;
+    // if (a % b >= 0.5) {
+      // c = b + 1;
+    // }
+    // else {
+      // c = b;
+    // }
+    // System.out.println(c);
+    /// N6
+    // float a = 1.32F;
+    // float b = 9.84F;
+    // float k = 1.0F;
+    // System.out.println(a * b / k + " - ab ფართობის მქონე მართკუთხედს გაავსებს ამდენი k^2 ფართობის მქონე კვადრატი.");
+    /// N7
+    // int totalSeconds = 8908;
+    // int hours = 0;
+    // int minutes = 0;
+    // int remainingSeconds = totalSeconds;
+   //  if (remainingSeconds >= 3600) {
+       //  hours = remainingSeconds / 3600;
+       //  remainingSeconds -= hours * 3600;
+    // }
+    // if (remainingSeconds >= 60) {
+       //  minutes = remainingSeconds / 60;
+       //  remainingSeconds -= minutes * 60;
+    // }
+    // System.out.println(hours + " hours " + minutes + " minutes " + remainingSeconds + " seconds.");
+    /// N8
+    // int hours;
+    // int minutes;
+    // int seconds = 8908;
+    // hours = seconds / 3600;
+    // minutes = (seconds % 3600 - hours) / 60;
+    // int remainingSeconds = seconds % 60; ;
+    // System.out.println(hours + " hours " + minutes + " minutes " + remainingSeconds + " seconds.");
+  }
+}
